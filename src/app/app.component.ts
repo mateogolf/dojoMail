427e8259ff490af79f543c54237272e6a477cb1b
mateogolf <rodriguezml11@mail.wlu.edu>
2017-10-11 12:01:37 -0400

    changed importance to a boolean and used ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,24 @@ export class AppComponent {
   title = 'app';
   emails = [
     {email:'bill@gates.com',
-    importance:'High',
+    importance:true,
     subject: 'New Windows',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel neque suscipit, ornare leo quis, pharetra tellus. Pellentesque quis fringilla ipsum, sit amet ultricies mauris. Duis porta non eros eu vehicula. Sed molestie posuere ullamcorper. Aenean tempor urna sed interdum sagittis.'},
     {
       email: 'matt@gates.com',
-      importance: 'High',
+      importance: true,
       subject: 'New Windows',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel neque suscipit, ornare leo quis, pharetra tellus.'
     },
     {
       email: 'cstooge@gates.com',
-      importance: 'Low',
+      importance: false,
       subject: 'Wuwuwuwuwuwu',
       content: 'Pellentesque quis fringilla ipsum, sit amet ultricies mauris. Duis porta non eros eu vehicula. '
     },
     {
       email: 'mstooge@gates.com',
-      importance: 'Low',
+      importance: false,
       subject: 'You idiot!!',
       content: 'Pellentesque quis fringilla ipsum, sit amet ultricies mauris. Duis porta non eros eu vehicula. '
     },
